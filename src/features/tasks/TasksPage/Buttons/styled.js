490e55fd15bd5c05fd2420e96b1ce99460fdf702
Piotr Michalski <pipi6620@gmail.com>
2023-06-04 17:42:07 +0200

    moved Button

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -4,25 +4,3 @@ export const StyledButtons = styled.div`
     display: flex;
     flex-wrap: wrap;
 `;
-
-export const Button = styled.button`
-    color: ${({ theme }) => theme.colors.teal};
-    border: none;
-    background-color: transparent;
-    margin: 0 10px;
-    transition: 0.5s ease;
-
-    &:hover {
-        color: ${({ theme }) => theme.colors.bondiBlue};
-        cursor: pointer;
-    }
-
-    &:disabled {
-        color: ${({ theme }) => theme.colors.silver};
-    }
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-        flex-basis: 100%;
-        margin-bottom: 20px;
-    }
-`;
