483c24114bb90b9d4323de38422da2a0361cdbed
Piotr Michalski <pipi6620@gmail.com>
2023-06-04 17:36:14 +0200

    made navigation styled

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import AuthorPage from "./features/author/AuthorPage";
-import { StyledNavLink } from "./styled";
+import { StyledNavLink, Navigation, NavigationList, NavigationItem } from "./styled";
 
 export const App = () => (
-    <BrowserRouter basename="/to-do-list-react">
-        <nav>
-            <ul>
-                <li>
+    <HashRouter>
+        <Navigation>
+            <NavigationList>
+                <NavigationItem>
                     <StyledNavLink to="/zadania">Zadania</StyledNavLink>
-                </li>
-                <li>
+                </NavigationItem>
+                <NavigationItem>
                     <StyledNavLink to="/autor">O autorze</StyledNavLink>
-                </li>
-            </ul>
-            <Switch>
-                <Route path="/zadania/:id">
-                    <TaskPage />
-                </Route>
-                <Route path="/zadania">
-                    <TasksPage />
-                </Route>
-                <Route path="/autor">
-                    <AuthorPage />
-                </Route>
-                <Route path="/">
-                    <Redirect to="/zadania" />
-                </Route>
-            </Switch>
-        </nav>
-    </BrowserRouter>
+                </NavigationItem>
+            </NavigationList>
+        </Navigation>
+        <Switch>
+            <Route path="/zadania/:id">
+                <TaskPage />
+            </Route>
+            <Route path="/zadania">
+                <TasksPage />
+            </Route>
+            <Route path="/autor">
+                <AuthorPage />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
+        </Switch>
+    </HashRouter>
 );
--- src/styled.js
+++ src/styled.js
@@ -3,10 +3,30 @@ import { NavLink } from "react-router-dom"
 
 const activeClassName = "active";
 
+export const Navigation = styled.nav`
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.teal};
+`;
+
+export const NavigationList = styled.ul`
+    list-style: none;
+    margin-top: 0px;
+    padding: 15px;
+    display: flex;
+    justify-content: center;
+`;
+
+export const NavigationItem = styled.li`
+    margin: 0 20px;
+`;
+
 export const StyledNavLink = styled(NavLink).attrs(() => ({
     activeClassName,
 }))`
+    color: ${({ theme }) => theme.colors.sugarCane};
+    text-decoration: none;
+
     &.${activeClassName} {
-        color: red;
+        font-weight: bold;
     }
 `;
