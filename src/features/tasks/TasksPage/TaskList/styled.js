550e0ec95a9d224e0f106390e8da5677a6bbb0f6
Piotr Michalski <pipi6620@gmail.com>
2023-06-04 17:40:13 +0200

    added style for links

--- src/features/tasks/TasksPage/TaskList/styled.js
+++ src/features/tasks/TasksPage/TaskList/styled.js
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
 
 export const List = styled.ul`
     background-color: white;
@@ -57,5 +58,10 @@ export const Content = styled.span`
     ${({ done }) => done && css`
         text-decoration: line-through;
     `}
+`;
+
+export const StyledLink = styled(Link)`
+    color: ${({ theme }) => theme.colors.teal};
+    text-decoration: none;
 `;
 
